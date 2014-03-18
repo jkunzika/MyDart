898f23fe4ecdb43d8a85c06e0e5a08aaea5f8c54
Jordan Kunzika <Jordan.S.Kunzika.16@Dartmouth.edu>
2014-03-17 22:00:57 -0400

    formatted files with proper indents

--- fullscreen.js
+++ fullscreen.js
@@ -1,49 +1,49 @@
 $(document).ready(function(){
-var itemsOriginalWidth = $('.items').width();
-var itemsCurrentWidth = itemsOriginalWidth;
-var contentOriginalSize = $('.content').width();
-var $body = $('body'); //Cache this for performance
-			var diff = ($('#Accordion').width() - $('.content').width()) / 2;
-            var setContentScale = function() {
-            	
-                var scaleSource = $body.width(),
-                    scaleFactor = 0.75,                     
-                    maxScale = 1000,
-                    minScale = 500; //Tweak these values to taste
-
-                var contentSize = scaleSource * scaleFactor; //Multiply the width of the body by the scaling factor:
-                console.log("contentSize = " + contentSize);
-                console.log("diff = " + diff);
-                console.log("itemsOriginalWidth = " + itemsOriginalWidth);
-
-                if (contentSize > maxScale){
-                 contentSize = maxScale;
-                 }
-                if (contentSize < minScale) {
+	var itemsOriginalWidth = $('.items').width();
+	var itemsCurrentWidth = itemsOriginalWidth;
+	var contentOriginalSize = $('.content').width();
+	var $body = $('body'); //Cache this for performance
+	var diff = ($('#Accordion').width() - $('.content').width()) / 2;
+	var setContentScale = 
+	    function() {
+	    var scaleSource = $body.width();
+	    var scaleFactor = 0.75;                     
+	    var maxScale = 1000;
+	    var minScale = 500; //Tweak these values to taste
+	    
+	    var contentSize = scaleSource * scaleFactor; //Multiply the width of the body by the scaling factor:
+	    console.log("contentSize = " + contentSize);
+	    console.log("diff = " + diff);
+	    console.log("itemsOriginalWidth = " + itemsOriginalWidth);
+	    
+	    if (contentSize > maxScale){
+		contentSize = maxScale;
+	    }
+	    if (contentSize < minScale) {
                 contentSize = minScale; //Enforce the minimum and maximums
-                }
-                itemsCurrentWidth = $('.items').width();
-                var itemsScaleFactor = $(window).width() - $('#Accordion').width() - $('.items').width();
-                console.log("itemsScaleFactor = " + itemsScaleFactor);
-                if(contentSize > 500 || contentSize < 1000){
+	    }
+	    itemsCurrentWidth = $('.items').width();
+	    var itemsScaleFactor = $(window).width() - $('#Accordion').width() - $('.items').width();
+	    console.log("itemsScaleFactor = " + itemsScaleFactor);
+	    if(contentSize > 500 || contentSize < 1000){
                 $('.items').css('width', itemsCurrentWidth + itemsScaleFactor);
                 var resize = itemsCurrentWidth + itemsScaleFactor;
                 console.log("items resized to = " + resize);
-                }
-                else{
+	    }
+	    else{
                 $('.items').css('width', itemsOriginalWidth);
                 console.log("items resized to original width");
-                }
-                $('#Accordion').css('width', contentSize);// + '%');
-                $('.content').css('width', contentSize - diff);// + '%');
-
-                //$('body').css('font-size', fontSize + '%');
-            }
-
-            $(window).resize(function(){
+	    }
+	    $('#Accordion').css('width', contentSize);// + '%');
+	    $('.content').css('width', contentSize - diff);// + '%');
+	    
+	    //$('body').css('font-size', fontSize + '%');
+	}
+	
+	$(window).resize(function(){
                 setContentScale();
             });
-
-            //Fire it when the page first loads:
-            setContentScale();
-});
+	
+	//Fire it when the page first loads:
+	setContentScale();
+    });
